e60ed013bde7549e5443ed56d99903ecff8ee926
dpmungalpara1 <69724531+dpmungalpara1@users.noreply.github.com>
2020-08-15 11:23:43 -0400

    Add files via upload

---
+++ watch.js
@@ -0,0 +1,364 @@
+/** Add any JavaScript you need to this file. */
+window.productsData = [
+    {
+        id: "applewatch s3",
+        name: "Apple Watch Series3",
+        description: "Aluminium Case with White Sport Band. ",
+        price: "279.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Apple"
+        },
+        images: [
+            "apple_series31.jpg",
+            "apple_series32.jpg"
+           
+        ]
+    },
+    {
+        id: "applewatch s4",
+        name: "Apple Watch Series4",
+        description: "Stainless steel Case with Gold metal band.",
+        price: "459.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Apple"
+        },
+        images: [
+            "apple_series41.jpg",
+            "apple_series42.jpg"
+        ]
+    },
+    {
+        id: "applewatch s5",
+        name: "Apple Watch Series5",
+        description: "Aluminium Case with Black Sport Band",
+        price: "549.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Apple"
+        },
+        images: [
+            "apple_series51.jpg",
+            "apple_series52.jpg"
+        ]
+    },
+    {
+        id: "fastrack b",
+        name: "Fastrack Bob",
+        description:
+            "Aluminium case with Leather Band",
+        price: "60.99",
+        type: "analog",
+        categories: {
+            brand: "Fastrack"
+        },
+        images: [
+            "fastrack_bob1.jpg",
+            "fastrack_bob2.jpg"
+        ]
+    },
+    {
+        id: "fastrack raga",
+        name: "Fastrack Raga",
+        description:
+            "Stainless steel Analog dial with Golden metalic band.",
+        price: "79.99",
+        type: "analog",
+        categories: {
+            brand: "Fastrack"
+        },
+        images: [
+            "fastrack_raga1.jpg",
+            "fastrack_raga2.jpg"
+        ]
+    },
+    
+    {
+        id: "Samsung a1",
+        name: "Samsung Active1",
+        description: "Samsung Galaxy Watch Active 40mm Smartwatch with Heart Rate Monitor- Silver",
+        price: "259.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Samsung"
+        },
+        images: [
+            "samsung_active11.jpg",
+            "samsung_active12.jpg",
+            "samsung_active13.jpg"
+        ]
+    },
+    {
+        id: "Samsung a2",
+        name: "Samsung Active2",
+        description: "Samsung Galaxy Watch Active 40mm Smartwatch with Heart Rate Monitor- Aluminium",
+        price: "359.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Samsung"
+        },
+        images: [
+            "samsung_active21.jpg",
+            "samsung_active22.jpg"
+        ]
+    },
+    {
+        id: "samsung fit",
+        name: "Samsung Galaxy Fit",
+        description: "Samsung Galaxy Fit Smartwatch Fitness Band Stress & Sleep Tracker, AMOLED Display, 5ATM Water Resistance- Black.",
+        price: "129.99",
+        type: "smartband",
+        categories: {
+            brand: "Fitbit"
+        },
+        images: [
+            "samsung_fitbit1.jpg",
+            "samsung_fitbit2.jpg"
+        ]
+    },
+    {
+        
+        id: "samsung Watch",
+        name: "Samsung Galaxy Watch ",
+        description: "Samsung Galaxy Watch 46mm Smartwatch with Heart Rate Monitor- Silver/Black",
+        price: "369.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Samsung"
+        },
+        images: [
+            "samsung_galaxy1.jpg",
+            "samsung_galaxy2.jpg"
+        ]
+    },
+    {
+        id: "fitbit alta1",
+        name: "Fitbit Alta",
+        description: "Fitbit Alta fitness tracker Black large.",
+        price: "119.99",
+        type: "smartband",
+        categories: {
+            brand: "Fitbit"
+        },
+        images: [
+            "fitbit_alta1.jpg",
+            "fitbit_alta2.jpg",
+            "fitbit_alta3.jpg"
+        ]
+    },
+    {
+        id: "fitbit c1",
+        name: "Fitbit Charge1",
+        description: "Fitbit Charge Fitness tracker Black compact.",
+        price: "149.99",
+        type: "smartband",
+        categories: {
+            brand: "Fitbit"
+        },
+        images: [
+            "fitbit_charge11.jpg",
+            "fitbit_charge12.jpg"
+        ]
+    },
+    {
+        id: "fitbit v1",
+        name: "Fitbit Versa1",
+        description: "Fitbit Versa Lite Edition Smartwatch.",
+        price: "199.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Fitbit"
+        },
+        images: [
+            "fitbit_versa11.jpg",
+            "fitbit_versa12.jpg"
+        ]
+    },
+    {
+        id: "fitbit v2",
+        name: "Fitbit Versa2",
+        description: "Fitbit Versa 2 Smartwatch with Amazon Alexa & Heart Rate Tracking.",
+        price: "269.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Fitbit"
+        },
+        images: [
+            "fitbit_versa21.jpg",
+            "fitbit_versa22.jpg"
+        ]
+    },
+    {
+        id: "fossil c",
+        name: "Fossil Croco",
+        description: "Fossil Men's Townsman Automatic Brown Croco Leather Watch",
+        price: "199.99",
+        type: "analog",
+        categories: {
+            brand: "Fossil"
+        },
+        images: [
+            "fossil_croco1.jpg",
+            "fossil_croco2.jpg",
+            "fossil_croco3.jpg",
+        ]
+    },
+    {
+        id: "fossil f",
+        name: "Fossil Flynn",
+        description: "Fossil Men's Flynn Chronograph Black Stainless Steel Watch",
+        price: "109.99",
+        type: "analog",
+        categories: {
+            brand: "Fossil"
+        },
+        images: [
+            "fossil_flynn1.jpg",
+            "fossil_flynn2.jpg"
+        ]
+    },
+    {
+        id: "fossil g4",
+        name: "Fossil Gen4",
+        description: "Fossil Men's Fossil Sport Smartwatch Smoky Blue Silicone-Grey",
+        price: "239.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Fossil"
+        },
+        images: [
+            "fossil_gen41.jpg",
+            "fossil_gen42.jpg",
+            "fossil_gen43.jpg"
+        ]
+    },
+    {
+        id: "fossil g5",
+        name: "Fossil Gen5",
+        description: "Fossil Men's Fossil Sport Smartwatch Smoky Black Silicone",
+        price: "389.99",
+        type: "smartwatch",
+        categories: {
+            brand: "Fossil"
+        },
+        images: [
+            "fossil_gen51.jpg",
+            "fossil_gen52.jpg",
+            "fossil_gen53.jpg"
+        ]
+    },
+    {
+        id: "fossil rhett",
+        name: "Fossil Rhett",
+        description: "Fossil Men's Rhett Chronograph Navy Leather Watch -Blue",
+        price: "129.99",
+        type: "analog",
+        categories: {
+            brand: "Fossil"
+        },
+        images: [
+            "fossil_rhett1.jpg",
+            "fossil_rhett2.jpg"
+        ]
+    },
+    {
+        id: "mi 3",
+        name: "Mi Band3",
+        description:
+            "Mi Band 3 Smart Wristband Bracelet.",
+        price: "59.99",
+        type: "smartband",
+        categories: {
+            brand: "MI"
+        },
+        images: [
+            "mi_band31.jpg",
+            "mi_band32.jpg"
+        ]
+    },
+    {
+        id: "mi 4",
+        name: "Mi Band4",
+        description:
+            "Mi Band 4 Bluetooth 5.0 Music Smart Watch Bracelet.",
+        price: "89.99",
+        type: "smartband",
+        categories: {
+            brand: "MI"
+        },
+        images: [
+            "mi_band41.jpg",
+            "mi_band42.jpg"
+        ]
+    },
+    {
+        id: "sonata u",
+        name: "Sonata Ulysse",
+        description:
+            "Sonata Ulysse Cathedral Mens Wrist White Gold Watch.",
+        price: "279.99",
+        type: "analog",
+        categories: {
+            brand: "Sonata"
+        },
+        images: [
+            "sonata_ulysse1.jpg",
+            "sonata_ulysse2.jpg"
+        ]
+    },
+    {
+        
+        id: "titan e",
+        name: "Titan Epic",
+        description:
+            "Titan Epic Watch with Black Stainless Steel Dial.",
+        price: "79.99",
+        type: "analog",
+        categories: {
+            brand: "Titan"
+        },
+        images: [
+            "titan_epic1.jpg",
+            "titan_epic2.jpg"
+        ]
+    },
+    {
+        id: "titan v",
+        name: "Titan Vintage",
+        description:
+            "Titan Vintage Watch White Aluminium dial covered with leather Band.",
+        price: "149.99",
+        type: "analog",
+        categories: {
+            brand: "Titan"
+        },
+        images: [
+            "titan_vintage1.jpg",
+            "titan_vintage2.jpg"
+        ]
+    },
+    {
+        id: "titan vogue",
+        name: "Titan Vogue",
+        description:
+            "Titan Vogue Automatic Watch Stainless Steel Black Dial Wrapped By Leather Band.",
+        price: "189.99",
+        type: "analog",
+        categories: {
+            brand: "Titan"
+        },
+        images: [
+            "titan_vogue1.jpg",
+            "titan_vogue2.jpg"
+        ]
+    }
+];
+
+window.categoryLabels = {
+    type: "Device Type",
+    brand: "Brand"
+    
+    
+   
+};
